529268f9a1ec2889e48c123661d32bb1aa360322
manojvignesh <manojvignesh@live.com>
2020-09-14 04:53:00 +0000

    Initial commit

---
+++ theia-training/src/browser/theia-training-frontend-module.ts
@@ -0,0 +1,15 @@
+/**
+ * Generated using theia-extension-generator
+ */
+import { TheiaTrainingCommandContribution, TheiaTrainingMenuContribution } from './theia-training-contribution';
+import {
+    CommandContribution,
+    MenuContribution
+} from "@theia/core/lib/common";
+import { ContainerModule } from "inversify";
+
+export default new ContainerModule(bind => {
+    // add your contribution bindings here
+    bind(CommandContribution).to(TheiaTrainingCommandContribution);
+    bind(MenuContribution).to(TheiaTrainingMenuContribution);
+});
